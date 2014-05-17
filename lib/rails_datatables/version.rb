912069efba92fc5710239b3066d273538d15d240
Antonio Antillon <antillas21@gmail.com>
2014-05-16 19:26:28 -0700

    Adds a test suite via RSpec
    * Adds RSpec to test functionality.
    * Refactors gem into a namespaced environment to avoid collisions.
    * Renames base class to `RailsDatatables::Base`.
    * Edits base hook methods the base class provides.
    * Edits default generator to `DatatableGenerator`. Can be invoked with:
      `$ rails generate datatable NAME`

---
+++ lib/rails_datatables/version.rb
@@ -0,0 +1,3 @@
+module RailsDatatables
+  VERSION = "0.0.2"
+end
--- spec/rails-datatables/ajax_datatables_rails_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe RailsDatatables do
-  describe "as_json" do
-    it "should return the correct json feed"
-  end
-end
